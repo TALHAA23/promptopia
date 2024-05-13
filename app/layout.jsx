a5d25df755923ab7b938a42d2e4688938472aca0
Talha <talhasifat23@gmail.com>
2024-05-13 21:28:15 +0500

    May 13, 2024

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "@styles/globals.css";
 import Nav from "@components/Nav";
 import { useState, useEffect } from "react";
 import { signIn, signOut, getProviders, useSession } from "next-auth/react";
+import Provider from "@components/Provider";
 // export const metadata = {
 //   title: "promptopia",
 //   description: "Discover & Share AI Prompts",
@@ -11,13 +12,15 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,21 @@
-import { signOut } from "next-auth/react";
+import { signOut, signIn, getProviders, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Nav = () => {
   const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+  const [toggleDropDown, setToggleDropDown] = useState(false);
+
+  // useEffect(() => {
+  //   const getProviders = async () => {
+  //     const response = await getProviders();
+  //     setProviders(response);
+  //   };
+  //   getProviders();
+  // }, []);
+
   return (
     <div className="w-full flex-between mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -17,6 +29,7 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
+      {/* desktop navigation */}
       <div className="sm:flex hidden">
         {isUserLoggedIn ? (
           <div className="flex gap-2">
@@ -34,7 +47,76 @@ const Nav = () => {
             />
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+      {/* mobile navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="">
+            <Image
+              src="/assets/images/logo.svg"
+              alt="promptopia logo"
+              width={30}
+              height={30}
+              onClick={() => setToggleDropDown((prev) => !prev)}
+              className=" object-contain"
+            />
+            {toggleDropDown && (
+              <div className="dropdown">
+                <Link
+                  href="profile"
+                  onClick={() => setToggleDropDown(false)}
+                  className="dropdown_link"
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="create-prompt"
+                  onClick={() => setToggleDropDown(false)}
+                  className="dropdown_link"
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  className="mt-5 w-full black_btn"
+                  type="button"
+                  onClick={() => {
+                    setToggleDropDown(false);
+                    signOut();
+                  }}
+                >
+                  Sign out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
     </div>
